75c241038201fb0b271131a08e3f3afca2a6c705
mikekavouras <kavourasm@gmail.com>
2015-10-23 21:45:00 -0400

    remove sdwebimage

--- Podfile
+++ Podfile
@@ -2,4 +2,3 @@ use_frameworks!
 
 pod 'Parse', '~> 1.8'
 pod 'ParseUI', '~> 1.1'
-pod 'SDWebImage', '~>3.7'
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -55,11 +55,9 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods/Bolts.framework'
   install_framework 'Pods/Parse.framework'
   install_framework 'Pods/ParseUI.framework'
-  install_framework 'Pods/SDWebImage.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/Bolts.framework'
   install_framework 'Pods/Parse.framework'
   install_framework 'Pods/ParseUI.framework'
-  install_framework 'Pods/SDWebImage.framework'
 fi
